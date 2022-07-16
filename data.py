b243bcdaa2c39baec8376a7208ff0368fa06b485
ChaitanyaPTank <tankchaitanya333@gmail.com>
2022-07-16 20:52:15 +0530

    feat:added new images

--- data.py
+++ data.py
@@ -2,9 +2,6 @@ clients = [
     {'img': 'img/clients/180_bistro.webp'},
     {'img': 'img/clients/amfah.webp'},
     {'img': 'img/clients/javed_habib.webp'},
-    # {'img': 'img/clients/blanco.webp'},
-    # {'img': 'img/clients/intera_design.webp'},
-    # {'img': 'img/clients/kap.webp'},
     {'img': 'img/clients/kapadia.webp'},
     {'img': 'img/clients/linit.webp'},
     {'img': 'img/clients/lohana.webp'},
@@ -46,30 +43,33 @@ portfolios = [
     },
 ]
 
+
+for i in range(27):
+    portfolios.append({
+        'img': format(f'img/portfolio/new/{str(i+1).zfill(4)}.webp'),
+        'name': 'Nerul'
+    })
+
+
 testimonials = [
     {
         'feedback': '"I just wanted to say thank you so much Jagdishbhai and Vijaybhai for the tremendous   effort. Sapra Brothers did an excellent job and I really appreciate you two making this so hassle-free for me. I will absolutely refer Sapra Brothers to anyone and will call you again for my future projects."',
-
         'feedback_by': '- Himanshu Sekhar'
     },
     {
         'feedback': '"Wonderful! Brilliant!! Awesome!!! Thank you so much for turning our ordinary looking home into our family’s dream-come-true. Vijaybhai, your skills are commendable; you are such an artist and we are so glad we decided to give Sapra Brothers our complete home renovation contract. Thank you for working so hard, being so professional and completing the contract well ahead of its due-date."',
-
         'feedback_by': '- Viraj Subbarao'
     },
     {
         'feedback': '"I just wanted to let you know how excited and pleased we are with the masonry and tiling work that you recently completed at our home. The new tiled driveway and entrance far exceeds what we had imagined, while the complete re-alignment of the existing tiled walkway truly enhances the new driveway. Many thanks to you both and your team for a job well done!"',
-
         'feedback_by': '- Loy D\'souza'
     },
     {
         'feedback': '"In this day and age, it is good fortune, a joy and a comfort to deal with a contractor who not only takes pride in his work and is true to his word. Thank you! Both of you were great! Very professional team! We simply love the job done! You promised, you delivered!"',
-
         'feedback_by': '- Girish Choudhary'
     },
     {
         'feedback': '"We want to let you know how pleased we are with the team that performed the renovation work and redesigning of our living room and bathroom. They were professional in both their attitudes and workmanship. We are very grateful for the personal attention you gave to the job."',
-
         'feedback_by': '- Kedar Singh'
     }
 ]
--- main.py
+++ main.py
@@ -1,47 +1,17 @@
-from flask import Flask, render_template, request
-from flask.helpers import make_response, url_for
-from werkzeug.utils import redirect
+from flask import Flask, render_template
+from flask.helpers import make_response
 from data import clients, portfolios, testimonials
-# from flask_mail import Mail, Message
 import config
 
 
 MODE = True if config.ENV == "DEV" else False
-# ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
-# print(ASSETS_DIR)
 
 
 app = Flask(__name__)
 
 
-# mail = app.config.update(
-#     DEBUG=True,
-#     # EMAIL SETTINGS
-#     MAIL_SERVER='smtp.gmail.com',
-#     MAIL_PORT=465,
-#     MAIL_USE_SSL=True,
-#     MAIL_USERNAME='tankchaitanya333@gmail.com',
-#     MAIL_PASSWORD='@Akshardham1837'
-# )
-
-
-# mail = Mail(app)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    # if request.method == 'POST':
-    #     full_name = request.form.get('Full Name')
-    #     email = request.form.get('Email')
-    #     msg = request.form.get('Message')
-    #     msg = Message(
-    #         'Test',
-    #         sender='joister333@gmail.com',
-    #         recipients=[email]
-    #     )
-    #     msg.body = f'Hello {full_name}<br>Thanks for Contacting Us <br> We will revert Back to you soon'
-    #     mail.send(msg)
-    #     return redirect(url_for('/'))
     response = make_response(
         render_template(
             "base.html",
